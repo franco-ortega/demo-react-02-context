f512cafd588574d535f7b9b434f95376e2d7cc07
Franco Ortega <francopdx@gmail.com>
2021-11-11 14:38:39 -0800

    add increment button to Child

--- src/components/child/Child.js
+++ src/components/child/Child.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useCounterContext } from '../../state/CounterContext';
 import Grandchild from '../grandchild/Grandchild';
 
 const Child = () => {
+  const { incrementCount } = useCounterContext();
   
   return (
     <section>
       <h2>Child</h2>
+      <button onClick={incrementCount}>Plus One to Grandchild</button>
       <Grandchild />
     </section>
   )
