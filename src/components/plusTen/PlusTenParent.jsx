efae8c1be27d7d74e50b98d8b92baa5284497cba
Franco Ortega <francopdx@gmail.com>
2021-11-11 16:36:38 -0800

    update console messages

--- src/components/plusTen/PlusTenParent.jsx
+++ src/components/plusTen/PlusTenParent.jsx
@@ -7,7 +7,7 @@ const PlusTenParent = () => {
   return (
     <div>
       <h2>Plus Ten Parent</h2>
-      <p>(all children use the same custom hook but have independent state)</p>
+      <p>(each child uses the same custom hook, but they use it directly and separately, so their state is independent from each other)</p>
       <PlusTenChildOne />
       <PlusTenChildTwo />
       <PlusTenChildThree />
--- src/components/plusTwenty/PlusTwentyParent.jsx
+++ src/components/plusTwenty/PlusTwentyParent.jsx
@@ -8,13 +8,13 @@ const PlusTwentyParent = () => {
   const { plusTwenty, incrementByTwenty } = usePlusTwenty();
 
   useEffect(() => {
-    console.log('%c Hi hi hi from PlusTwentyParent on page load and when my any of my children update the state that I passed them via props! I am using a custom hook, but they are all getting the same state from me!!!', 'color: orangered');
+    console.log('%c Hi hi hi from PlusTwentyParent on page load and when my any of my children update the state that I passed them via props! I am using a custom hook, and they are all getting the same state from me!!!', 'color: orangered');
   }, [plusTwenty]);
 
   return (
     <div>
       <h2>Plus Twenty Parent</h2>
-      <p>(all children use the same custom hook state passed via props from the same parent)</p>
+      <p>(parent uses a custom hook for state and passes this state to its children via props, so they all share the same state)</p>
       <PlusTwentyChildOne plusTwenty={plusTwenty} incrementByTwenty={incrementByTwenty} />
       <PlusTwentyChildTwo plusTwenty={plusTwenty} incrementByTwenty={incrementByTwenty} />
       <PlusTwentyChildThree plusTwenty={plusTwenty} incrementByTwenty={incrementByTwenty} />
